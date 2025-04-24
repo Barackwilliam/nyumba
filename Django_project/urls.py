e8d090b40b455bab39a913434d47df1b29a20cae
barackwilliam <barackwilliam12@gmail.com>
2025-04-24 13:12:34 +0300

    add loading page

--- Django_project/urls.py
+++ Django_project/urls.py
@@ -2,12 +2,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from  django.conf import settings
+from Myapp.views import loading_page  # ile splash view
+
 
 from django.conf.urls.static import static 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Myapp.urls')),
+    path('', loading_page, name='loading'),
+    path('app/', include('Myapp.urls')),
     path('chat/', include('chat.urls')),
     path('Blog/', include('Blog.urls')),
     path('auth/', include('social_django.urls', namespace='social')),
