cba61d8caf6501b9d72451d9bf7e458a91575b8d
barackwilliam <barackwilliam12@gmail.com>
2025-04-25 09:34:16 +0300

    remove app loader

--- Django_project/urls.py
+++ Django_project/urls.py
@@ -9,8 +9,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', loading_page, name='loading'),
-    path('app/', include('Myapp.urls')),
+    # path('', loading_page, name='loading'),
+    path('', include('Myapp.urls')),
     path('chat/', include('chat.urls')),
     path('Blog/', include('Blog.urls')),
     path('auth/', include('social_django.urls', namespace='social')),
