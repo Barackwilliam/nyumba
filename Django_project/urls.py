bc4b954305f84130fe0a212184ea2b36677fbf49
barackwilliam <barackwilliam12@gmail.com>
2025-04-25 09:45:13 +0300

    remove app loader

--- Django_project/urls.py
+++ Django_project/urls.py
@@ -2,7 +2,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from  django.conf import settings
-from Myapp.views import loading_page  # ile splash view
 
 
 from django.conf.urls.static import static 
