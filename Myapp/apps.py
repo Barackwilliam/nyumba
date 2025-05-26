251ffe94df5dcd159cdb0eabbd60d6bf0fddf38c
barackwilliam <barackwilliam12@gmail.com>
2025-05-26 17:46:23 +0300

    set to remove no profile message

--- Myapp/apps.py
+++ Myapp/apps.py
@@ -1,11 +1,8 @@
-
-
-# Myapp/apps.py
 from django.apps import AppConfig
-from django.utils.module_loading import autodiscover_modules
 
 class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Myapp'
 
-  
+    def ready(self):
+        import Myapp.signals  # Hii inahakikisha signals zinaanzishwa
